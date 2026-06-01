9ed917cdf2c357507ead423930ac727431d9dcb4
SrslyKail <90731959+SrslyKail@users.noreply.github.com>
2026-06-01 11:39:21 -0700

    Add movement events and controls

--- Assets/Scripts/Core/Player/PlayerMovement.cs
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private InputReader inputReader;
@@ -8,10 +9,57 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [Header("Settings")]
     [SerializeField] private float movementSpeed;
-    [SerializeField] private float turnRate;
+    [SerializeField] private float turnRateDegPerSecond;
+
+    private Vector2 _previousMovementInput;
     
+    //Not using Start so we ensure we don't access data until networking is set up
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            return; 
+        }
+
+        inputReader.MoveEvent += HandleMove;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner)
+        {
+            return; 
+        }
+        
+        inputReader.MoveEvent -= HandleMove;
+        
+    }
+
+    private void HandleMove(Vector2 movementInput)
+    {
+        _previousMovementInput = movementInput;
+    }
+
     private void Update()
     {
-        
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        //Multiplay by negative turn rate, otherwise controls are backwards
+        float zRotation = _previousMovementInput.x * -turnRateDegPerSecond * Time.deltaTime;
+        bodyTransform.Rotate(0f, 0f, zRotation);
+    }
+
+    private void FixedUpdate()
+    {
+        //Update RB here so we stay in sync with physics updates
+        if (!IsOwner)
+        {
+            return;
+        }
+        //Apply forwards/backwards forces
+        rb.linearVelocity = (Vector2)bodyTransform.up * (_previousMovementInput.y * movementSpeed);
     }
 }
