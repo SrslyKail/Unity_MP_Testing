cab44c63a3a5bca1b1de8dc442fa5be4ec9b9bce
SrslyKail <90731959+SrslyKail@users.noreply.github.com>
2026-06-03 10:21:40 -0700

    Add healthbar

---
+++ Assets/Scripts/Core/Combat/HealthDisplay.cs
@@ -0,0 +1,35 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : NetworkBehaviour
+{
+    [Header("References")] [SerializeField]
+    private Health health;
+
+    [SerializeField] private Image healthBarImage;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsClient)
+        {
+            return;
+        }
+        health.CurrentHealth.OnValueChanged += HandleHealthChange;
+        HandleHealthChange(0, health.MaxHealth);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsClient)
+        {
+            return;
+        }
+        health.CurrentHealth.OnValueChanged -= HandleHealthChange;
+    }
+
+    private void HandleHealthChange(int oldHealth, int newHealth)
+    {
+        healthBarImage.fillAmount = (float)newHealth / health.MaxHealth;
+    }
+}
