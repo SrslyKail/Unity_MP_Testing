1dba1876debdae656c8562f442a96a876a292fdf
SrslyKail <90731959+SrslyKail@users.noreply.github.com>
2026-06-03 10:06:45 -0700

    Create health component

---
+++ Assets/Scripts/Core/Combat/Health.cs
@@ -0,0 +1,49 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    [field: SerializeField] public int MaxHealth { get; private set; } = 100;
+    public NetworkVariable<int> CurrentHealth = new NetworkVariable<int>();
+    
+    private bool _isDead = false;
+
+    public Action<Health> OnDeath;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer)
+        {
+            return;
+        }
+        
+        CurrentHealth.Value = MaxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        ModifyHealth(-damage);
+    }
+
+    public void RestoreHealth(int heal)
+    {
+        ModifyHealth(heal);
+    }
+
+    private void ModifyHealth(int value)
+    {
+        if (_isDead)
+        {
+            return;
+        }
+        int newHealth = CurrentHealth.Value + value;
+        CurrentHealth.Value = Mathf.Clamp(newHealth, 0, MaxHealth);
+        
+        if (CurrentHealth.Value == 0)
+        {
+            OnDeath?.Invoke(this);
+            _isDead = true;
+        }
+    }
+}
